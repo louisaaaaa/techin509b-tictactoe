3aee8e0ef4c429297b1cb20f581425908eb54bf7
Louisa Shi <louisa_shi@126.com>
2023-10-27 10:18:53 -0700

    Add logic and checks for tic-tac-toe game

--- cli.py
+++ cli.py
@@ -2,18 +2,60 @@
 # the Tic-Tac-Toe game. This is where input and output happens.
 # For core game logic, see logic.py.
 
-from logic import make_empty_board
+from logic import make_empty_board, get_winner
 
 
 # Reminder to check all the tests
 
+# print board function
+def print_board(board):
+     for line in board:
+            print("[", end="")
+            for item in line:
+                print(item, end="")
+            print("]")
+            
 if __name__ == '__main__':
     board = make_empty_board()
     winner = None
+    turn = True # Keep track of whose turn it is
+    count = 0 # See if draw
+    print("Welcome to Tic-Tac-Toe!")
     while winner == None:
-        print("TODO: take a turn!")
+        if count == 9:
+            print_board(board)
+            print("It's a Draw :D")
+            break
+        if turn:
+            print("X's turn!")
+        else:
+            print("O's turn!")
         # TODO: Show the board to the user.
+        print_board(board)
         # TODO: Input a move from the player.
-        # TODO: Update the board.
-        # TODO: Update who's turn it is.
-        winner = 'X'  # FIXME
+        try:
+            x,y = input("Input x y axis, use space to seperate: ").split()
+        except:
+            print("Input error, try again")
+            continue
+        
+        if int(x) >= 0 and int(x) <= 2 and int(y) >= 0 and int(y) <= 2: # check if range is in board   
+            if  board[int(x)][int(y)] != " ": # already has value
+                print("Already has value, please try to place at empty spot")
+                continue
+            # TODO: Update the board.
+            if turn: 
+                board[int(x)][int(y)] = 'X'
+            else:
+                board[int(x)][int(y)] = 'O'
+            # TODO: Update who's turn it is.
+            turn = not turn
+            count += 1
+            # See if anyone wins
+            winner = get_winner(board)
+        else: # out of range, redo
+            print("Input axis out of range, please try again")
+     
+    if winner != None:       
+        print_board(board)
+        print(f'{winner} wins!')
--- logic.py
+++ logic.py
@@ -5,16 +5,36 @@
 
 def make_empty_board():
     return [
-        [None, None, None],
-        [None, None, None],
-        [None, None, None],
+        [" ", " ", " "],
+        [" ", " ", " "],
+        [" ", " ", " "],
     ]
 
 
 def get_winner(board):
     """Determines the winner of the given board.
     Returns 'X', 'O', or None."""
-    return None  # FIXME
+    # Check horizontal
+    if board[0][0] == board[0][1] == board[0][2] and board[0][0] != " ":
+        return(board[0][0])
+    if board[1][0] == board[1][1] == board[1][2] and board[1][0] != " ":
+        return(board[1][0])
+    if board[2][0] == board[2][1] == board[2][2] and board[2][0] != " ":
+        return(board[2][0])
+    # Check vertical
+    if board[0][0] == board[1][0] == board[2][0] and board[0][0] != " ":
+        return(board[0][0])
+    if board[0][1] == board[1][1] == board[2][1] and board[0][1] != " ":
+        return(board[0][1])
+    if board[0][2] == board[1][2] == board[2][2] and board[0][2] != " ":
+        return(board[0][2])
+    # Check diagnal
+    if board[0][0] == board[1][1] == board[2][2] and board[0][0] != " ":
+        return(board[1][1])
+    if board[2][0] == board[1][1] == board[0][2] and board[2][0] != " ":
+        return(board[1][1])
+    
+    return None
 
 
 def other_player(player):
